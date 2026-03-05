89c3ba11037126190f5ebc67cbf0c0c5738e6ce0
Nina Preußler <nina.preussler@studserv.uni-leipzig.de>
2026-03-05 17:45:37 +0100

    add file to define functions

---
+++ custom_functions.py
@@ -0,0 +1,25 @@
+# Definitions of custom functions used across notebooks
+
+# 1. function to filter xarray file spatially based on shapefile
+import geopandas as gpd
+import xarray as xr
+
+def sel_bounds(xr_obj, mask):
+    
+    """Clip a rioxarray array/dataset to the bounding coordinates of a shapefile or another rioxarray object"""
+
+    if isinstance(mask, gpd.GeoDataFrame):
+        xmin = mask.bounds.minx.item()
+        xmax = mask.bounds.maxx.item()
+        ymin = mask.bounds.miny.item()
+        ymax = mask.bounds.maxy.item()
+
+    elif isinstance(mask, (xr.DataArray, xr.Dataset)):
+        xmin = mask.x.values.min()
+        xmax = mask.x.values.max()
+        ymin = mask.y.values.min()
+        ymax = mask.y.values.max()
+    
+    clipped = xr_obj.sel(x=slice(xmin, xmax), y=slice(ymax, ymin))
+    
+    return clipped
